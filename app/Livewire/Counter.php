81715a060b22f6aca842a2c7995559783a719095
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2023-10-17 14:33:46 +0630

    form and validation

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -2,11 +2,35 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Counter extends Component
 {
-    public $count = 0;
+    public $count    = 0;
+
+    #[Rule('required|min:3')] 
+    public $name     = "";
+
+    #[Rule('required|email|unique:users')]
+    public $email    = "";
+
+    #[Rule('required|min:6')]
+    public $password = "";
+
+    public function createNewUser()
+    {
+        $validated = $this->validate();
+
+        User::create([
+            'name'     => $this->name,
+            'email'    => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+    }
 
     public function increment()
     {
@@ -20,6 +44,10 @@ class Counter extends Component
 
     public function render()
     {
-        return view('livewire.counter');
+        $users = User::all();
+
+        return view('livewire.counter', [
+            'users' => $users,
+        ]);
     }
 }
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,5 +1,14 @@
 <div>
-    <h1>Count {{ $count }}</h1>
-    <button wire:click="increment">+</button>
-    <button wire:click="decrement">-</button>
+    <form action="" wire:submit="createNewUser">
+        <input wire:model="name" type="text" placeholder="name">
+        @error('name') {{ $message }} @endError
+        <input wire:model="email" type="email" placeholder="email">
+        <input wire:model="password" type="password" placeholder="password">
+
+        <button>Create</button>
+    </form>
+
+    @foreach($users as $user)
+        <p>{{ $user->name }}</p>
+    @endforeach
 </div>
